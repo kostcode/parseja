7626e04fbe44d25578e93cd7391eaa46f86c1462
A.K. <kurashiki@yandex.ru>
2026-02-02 15:33:24 +0300

    Fixed fugashu unidic install. Added SURU filter.

--- parse_ja_sample.py
+++ parse_ja_sample.py
@@ -1,4 +1,4 @@
-# japanese_filter.py
+
 import fugashi
 
 # Reuse the Tagger (it has initialization cost)
@@ -10,13 +10,17 @@ DROP_POS_PREFIXES = (
     "助詞",      # particles
     "助動詞",    # auxiliary verbs
     "補助記号",  # punctuation/symbols
+    "非自立可能"
 )
 
 def content_tokens(text: str):
     kept = []
-    for tok in tagger(text):
-        pos = tok.feature.pos  # UniDic POS string [page:2]
-        if any(pos.startswith(p) for p in DROP_POS_PREFIXES):
+    for tok in tagger(text):        
+        pos1 = tok.feature.pos1  # UniDic POS string [page:2]
+        pos2 = tok.feature.pos2  # UniDic POS string [page:2]
+        if any(pos1.startswith(p) for p in DROP_POS_PREFIXES):
+            continue
+        elif any(pos2.startswith(p) for p in DROP_POS_PREFIXES):
             continue
         kept.append(tok.surface)  # surface form [page:2]
     return kept
